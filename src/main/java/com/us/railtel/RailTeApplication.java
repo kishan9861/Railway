e1cf25b4e43a9f9290633cb5344f5213010c7c32
kishan <silukishan9861@gmail.com>
2025-01-29 00:33:56 +0530

    intial project

---
+++ src/main/java/com/us/railtel/RailTeApplication.java
@@ -0,0 +1,13 @@
+package com.us.railtel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RailTeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RailTeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/us/railtel/RailTeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.us.railtel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RailTeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
